ce1fc63e044290e67d4f2f1c1ae74fd7da755757
NomadicB <brandon@securitynomads.com>
2026-03-17 10:48:19 -0500

    learned about making sure input variable that is a number gets corrected into an int

---
+++ PhaseOne/userInfo.py
@@ -0,0 +1,6 @@
+user_name = input("What is your name?")
+birth_year = int(input("What year were you born?"))
+age = (2025 - birth_year)
+favorite_number = input("What is your favorite number?")
+
+print(f"Hello {user_name} you are {age} years old, and your favorite number is {favorite_number}")
