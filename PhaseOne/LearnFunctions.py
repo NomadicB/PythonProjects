91c5b4c14b9efba387d77d432e79cad52bf80316
NomadicB <brandon@securitynomads.com>
2026-03-18 14:28:15 -0500

    wrote small functions to go over some of what I learned today

---
+++ PhaseOne/LearnFunctions.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    total = a + b 
+    return total
+
+def multiply(a, b):
+    total = a + b
+    return total
+
+def greet(name):
+    message = "Hello, " + name
+    return message
+
+print(add(25, 5))
+print(multiply(5, 5))
+print(greet("Apprentice"))
