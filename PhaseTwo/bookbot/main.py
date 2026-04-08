7c4a2a645560449ea5878e2e348cfc7757498404
NomadicB <brandon@securitynomads.com>
2026-04-07 23:41:16 -0500

    added word count function

--- PhaseTwo/bookbot/main.py
+++ PhaseTwo/bookbot/main.py
@@ -2,10 +2,15 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def text_to_string(text):
+    split_text = len(text.split())
+    return split_text
+
 
 def main():
-    book_content = get_book_text("/home/bones/projects/bookbot/books/frankenstein.txt")
-    print(book_content)
+    book_content = get_book_text("/home/bones/projects/PythonBootcamp/PhaseTwo/bookbot/books/frankenstein.txt")
+    word_count = text_to_string(book_content)
+    print(f"Found {word_count} total words")
 
 
 main()
